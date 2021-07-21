6ccdecbeb8f96be8d66470d732ea009b97418585
Mohit Gayatri <mohitgayatri96@gmail.com>
2021-07-21 11:35:16 +0530

    Updated to test.py to show example of how to use the tool

--- src/test.py
+++ src/test.py
@@ -1,4 +1,5 @@
-import urllib.request, json 
-with urllib.request.urlopen("https://data.sec.gov/api/xbrl/companyconcept/CIK0001341439/us-gaap/AccountsPayableCurrent.json") as url:
-    oracle_data = json.loads(url.read().decode())  
-    print(oracle_data)
+from DCF import DCF
+
+apple_dcf = DCF("aapl")
+apple_dcf.create_excel_sheet("apple_dcf.xlsx")
+print(apple_dcf.get_npv_outputs()['Value per Share'])
