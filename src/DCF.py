d91b028c94b43ad092b91d81b5bcbab90b687010
Mohit Gayatri <mohitgayatri96@gmail.com>
2021-08-10 16:45:44 +0530

    add comments

--- src/DCF.py
+++ src/DCF.py
@@ -6,6 +6,7 @@ from spreadsheet import spreadsheet
 from compute import compute
 
 class DCF:
+    "Class to create DCF model given stock ticker and optional inputs"
     def __init__(self, ticker, form_type="10-K", num_past_filings=1, num_years=10):
         self.ticker = ticker
         self.num_years = 10
@@ -13,14 +14,26 @@ class DCF:
         # only supports 10-K with 1 past filing right now
         self.inputs = inputs.get_inputs(ticker, "10-K", 1)
     
+    # Internally compute DCF model for the given stock
+    # Creates python lists for all cashflows and return npv outputs
+    # @return dictionary containing NPV outputs such as Sum of PV, 
+    # value of equity, etc. (see compute_npv_outputs in compute.py)  
     def compute_npv_outputs(self):
-        npv = compute(self.ticker, self.inputs, self.num_years)
+        npv = compute(self.ticker, self.inputs, self.num_years, self.rates)
         return npv.compute_npv_outputs()
 
+    # Generates Excel Sheet containing DCF model for given stock
+    # Uses Ashwath Damodaran DCF as template for DCF model
+    # See "fcffsimpleginzu.xlsx" here for DCF model: 
+    # https://pages.stern.nyu.edu/~adamodar/New_Home_Page/spreadsh.htm
+    # @param filename - where to save excel file
     def create_excel_sheet(self, filename): 
         sheet = spreadsheet(self.ticker, self.inputs, self.num_years, self.rates)
         sheet.create_excel_sheet(filename)
     
+    # set default rates used in DCF model (see constants.py - default rates)
+    # @param **new_rates - dictionary containing key-value pairs of 
+    # rate being set and new rate value
     def set_rates(self, **new_rates):
         for key, value in new_rates.items():  
             if(key in self.rates): 
@@ -29,6 +42,7 @@ class DCF:
                 print("Could not find rate parameter: %s" % key)
                 print("Check constants.py for the correct name")
 
+    # resets rates to defaults defined in constants.py
     def reset_rates(self): 
         self.rates = constants.Default_rates
 
--- src/test.py
+++ src/test.py
@@ -3,13 +3,13 @@ from DCF import DCF
 tickers = ["ddog", "twlo"]
 for i in tickers: 
     dcf = DCF(i)
-    rates = {
-        "operating_margin_1_year": 10.0, 
-        "target_pretax_operating_margin": 60.0, 
-        "marginal_tax_rate": 14.0,
-        "growth_rate_1_year": 30.0,
-        "growth_rate_2_to_5_year": 25.0
-    }
-    dcf.set_rates(**rates)
+    #rates = {
+    #    "operating_margin_1_year": 10.0, 
+    #    "target_pretax_operating_margin": 60.0, 
+    #    "marginal_tax_rate": 14.0,
+    #    "growth_rate_1_year": 30.0,
+    #    "growth_rate_2_to_5_year": 25.0
+    #}
+    #sdcf.set_rates(**rates)
     dcf.create_excel_sheet(i)
     print("%.2f" % dcf.compute_npv_outputs()['Value per Share'])
